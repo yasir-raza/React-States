283ff182a725390bf0a39fce32420e568ab4e5fa
Yasir Raza <raza142010@gmail.com>
2020-06-09 23:08:34 +0500

    state changes

--- src/Room.js
+++ src/Room.js
@@ -5,6 +5,8 @@ import React, {useState} from 'react';
 function Room() {
     let [isLit, setLit] = useState(false);
     let [age, setAge] = useState(20);
+    let [light, setLight] = useState(false);
+    let [temp, setTemp] = useState(72);
     function updateLit(){
         setLit(!isLit)
     }
@@ -17,6 +19,18 @@ function Room() {
         <button onClick={updateLit}>Toggle Button</button>
         <br />
         <button onClick={() => setAge(++age)}>Increase Age</button>
+        <br />
+        light is {JSON.stringify(light)}
+        <br />
+        <button onClick={() => setLight(true)}>ON</button>
+        <button onClick={() => setLight(false)}>OFF</button>
+
+        light is {JSON.stringify(light)}
+        <br />
+        Room Temperature : {temp}
+        <br />
+        <button onClick={() => setTemp(temp+1)}>+</button>
+        <button onClick={() => setTemp(temp-1)}>-</button>
     </div>
   );
 }
